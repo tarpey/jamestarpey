9f94f9f677dae29de275123c1e9b445aa7bdd62d
James Tarpey <hello@jamestarpey.com>
2020-06-12 11:13:58 +0100

    Updates

---
+++ precache-manifest.58c9f04088f578ce6eff04693de053db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1183dd4d1e2468b6994ad5ade04e934c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc35cb30a679df7f8025",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d3688741544027128309",
+    "url": "/static/css/main.33c09ace.chunk.css"
+  },
+  {
+    "revision": "dc35cb30a679df7f8025",
+    "url": "/static/js/2.4344d6e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4344d6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3688741544027128309",
+    "url": "/static/js/main.51531a99.chunk.js"
+  },
+  {
+    "revision": "8c2be5901bb25dc07503",
+    "url": "/static/js/runtime-main.86955c97.js"
+  },
+  {
+    "revision": "32cc591e3e4bad1fe043dafb7fa30e92",
+    "url": "/static/media/photo.32cc591e.png"
+  }
+]);
