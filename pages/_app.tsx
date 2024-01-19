cbbc65a0e325656791525b4c1796524e82029a49
James Tarpey <hello@jamestarpey.com>
2024-01-19 12:33:58 +0000

    Updated website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import "../styles/main.scss";
 import type { AppProps } from "next/app";
 import { Header, Footer } from "../components/";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
       <Header />
       <div className="container">
         <Component {...pageProps} />
