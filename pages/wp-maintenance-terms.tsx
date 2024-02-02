b47fd8064fd676d7b7d82adccaf9407b33ea9ed5
James Tarpey <hello@jamestarpey.com>
2024-02-02 19:27:05 +0000

    Updated terms

--- pages/wp-maintenance-terms.tsx
+++ pages/wp-maintenance-terms.tsx
@@ -6,18 +6,26 @@ export default function Home() {
     <main>
       <Button icon="mdiArrowLeft" text="Back" url="/services" />
       <h1>Maintenance Terms</h1>
-      <p className="description">
-        You&apos;re agreeing to the terms below when subscribing for website
-        maintenance.
-        <br />
+      <p>
+        You&apos;re agreeing to the terms on this page when subscribing for
+        website maintenance services.
       </p>
+
+      <div>
+        <p>
+          For complete transparency, you should know that I have a full-time job
+          so I&apos;m not always contactable by phone. I&apos;ll always aim to
+          resolve any critical issues you raise within 24 hours, but I
+          can&apos;t 100% guarantee it.
+        </p>
+      </div>
 
       <div>
         <h3>What&apos;s included:</h3>
         <ul>
           <li>Weekly core, plugin and theme updates</li>
-          <li>Daily database and file backups</li>
-          <li>Restoring previous backups should any updates cause problems</li>
+          <li>Weekly database and file backups to the cloud</li>
+          <li>Restoring previous backups, should any updates cause issues</li>
           <li>
             Installation of a{" "}
             <a
@@ -57,7 +65,7 @@ export default function Home() {
               Search Engine Optimisation plugin
             </a>
           </li>
-          <li>Monthly call for around 15 minutes (optional)</li>
+          <li>Monthly call for around 15 minutes - completely optional</li>
         </ul>
         <h3>What&apos;s NOT included</h3>
         <ul>
@@ -83,7 +91,7 @@ export default function Home() {
           <a href="https://stripe.com/" rel="noreferrer" target="_blank">
             Stripe
           </a>{" "}
-          handles subscriptions, payments and invoices.
+          processes payments and handles invoices.
         </p>
         <p>
           You can login to the{" "}
@@ -102,8 +110,18 @@ export default function Home() {
       <div>
         <h3>Cancellation</h3>
         <p>
-          You can cancel at any time. The service continues until the end of the
-          billing cycle.
+          You can cancel at any time. The service will continue to the end of
+          the billing cycle.
+        </p>
+      </div>
+
+      <div>
+        <h3>Changes</h3>
+        <p>
+          I reserve the right to change how maintenance is provided. You&apos;ll
+          always be notified and have the opportunity to continue or cancel. I
+          may periodically recommend additional services if they&apos;ll be of
+          benefit to you or your website.
         </p>
       </div>
 
